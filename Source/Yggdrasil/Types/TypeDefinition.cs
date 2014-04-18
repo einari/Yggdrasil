29fd7bf1de6da53e20fd1d968eb50e3cf35e2cbe
Einar Ingebrigtsen <einar@dolittle.com>
2014-04-18 18:59:42 +0200

    TypeDefiniton for WinFX / Win8 store app done

--- Source/Yggdrasil/Types/TypeDefinition.cs
+++ Source/Yggdrasil/Types/TypeDefinition.cs
@@ -1,50 +1,55 @@
 ﻿using System;
+using System.Reflection;
+using System.Linq;
 
 namespace Yggdrasil.Types
 {
     public class TypeDefinition : ITypeDefinition
     {
-        public TypeDefinition()
+        TypeInfo    _typeInfo;
+
+
+        public TypeDefinition(Type type)
         {
+            Type = Type;
+            _typeInfo = type.GetTypeInfo();
         }
 
 
         public Type Type { get; private set; }
 
-        public string Namespace { get; private set; }
+        public string Namespace { get { return Type.Namespace; } }
+        public bool IsValueType { get { return _typeInfo.IsValueType; } }
+        public bool IsInterface { get { return _typeInfo.IsInterface; } }
+        public int ConstructorCount { get { return _typeInfo.DeclaredConstructors.Count(); } }
+        public bool HasConstructor { get { return ConstructorCount >= 1; } }
 
-        public bool IsValueType { get; set; }
-        
+        public bool HasDefaultConstructor { get { return _typeInfo.DeclaredConstructors.Any(c => c.GetParameters().Length == 0); } }
+        public bool HasSingletonAttribute { get { return _typeInfo.GetCustomAttributes(typeof(SingletonAttribute), true).Count() == 1; } }
 
-        public bool HasDefaultConstructor { get; set; }
-
-        public bool HasSingletonAttribute { get; set; }
+        public bool HasConstructorParametersValueTypes { get { return _typeInfo.DeclaredConstructors.Where(c => c.GetParameters().Any(p => p.ParameterType.GetTypeInfo().IsValueType)).Count() >= 0; } }
 
         public bool HasConstructorWithParameterTypes(params Type[] parameterTypes)
         {
-            throw new NotImplementedException();
+            return _typeInfo.DeclaredConstructors.Where(c => c.GetParameters().Select(p => p.ParameterType).SequenceEqual(parameterTypes)).Count() == 1;
+            
         }
 
         public object CreateInstance()
         {
-            throw new NotImplementedException();
+            return Activator.CreateInstance(Type);
         }
 
         public object CreateInstance(object[] parameters)
         {
-            throw new NotImplementedException();
+            return Activator.CreateInstance(Type, parameters);
         }
-
-
-        public bool IsInterface { get; private set; }
-        public int ConstructorCount { get; private set; }
-        public bool HasConstructor { get; private set; }
-
-        public bool HasConstructorParametersValueTypes { get; private set; }
+        
 
         public Type[] GetParameterTypesForFirstConstructor()
         {
-            throw new NotImplementedException();
+            var parameterTypes = _typeInfo.DeclaredConstructors.First().GetParameters().Select(p => p.ParameterType).ToArray();
+            return parameterTypes;
         }
     }
 }
