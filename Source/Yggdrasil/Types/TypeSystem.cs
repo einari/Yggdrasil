da044a024c6e60b34d3cb36b1e41495b03ebf792
Einar Ingebrigtsen <einar@dolittle.com>
2014-04-18 19:02:12 +0200

    TypeSystem for WinFX / Win8 Store apps up and running!

--- Source/Yggdrasil/Types/TypeSystem.cs
+++ Source/Yggdrasil/Types/TypeSystem.cs
@@ -1,18 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Yggdrasil.Types
 {
     public class TypeSystem : ITypeSystem
     {
+        Dictionary<Type, TypeDefinition> _typeDefinitions = new Dictionary<Type, TypeDefinition>();
+
         public ITypeDefinition GetDefinitionFor(Type type)
         {
-            throw new System.NotImplementedException();
+            if (_typeDefinitions.ContainsKey(type)) return _typeDefinitions[type];
+            var typeDefinition = new TypeDefinition(type);
+            _typeDefinitions[type] = typeDefinition;
+            return typeDefinition;
         }
 
 
         public Type GetType(string fullName)
         {
-            throw new NotImplementedException();
+            return Type.GetType(fullName);
         }
     }
 }
